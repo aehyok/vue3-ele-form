af35434bc3641ed6a82488ed5645ac34f401e8aa
刘启明 <aehyok@163.com>
2021-05-02 20:02:13 +0800

    重新初始化项目

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue';
-import App from './App.vue';
+import { createApp } from 'vue'
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
 
-createApp(App).mount('#app');
+const app = createApp(App);
+app.use(ElementPlus);
+app.use(store);
+app.use(router);
+app.mount('#app');
+// createApp(App).use(store).use(router).mount('#app')
