b1acc3c4d20ef0212af629f0a42d0e25543546ba
刘启明 <455043818@qq.com>
2021-05-10 09:41:50 +0800

    feat: 设置本地demo运行的固定port端口

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 module.exports = {
+  devServer: {
+    port: 5555,
+    proxy: {
+      '/offiac': {
+        // target: 'http://localhost:4000',
+        // target: 'http://mozbert.ngrok2.xiaomiqiu.cn',
+        // target: 'https://sykj.huaweiopenlab.com',
+        target: 'https://dvs-dev.sunlight-tech.com',
+        ws: true,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/offiac': '',
+        },
+      },
+    },
+  },
 //......上面一堆配置
   productionSourceMap: process.env.NODE_ENV === 'dev',
 //......
