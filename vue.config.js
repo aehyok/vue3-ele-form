0bf8db701c473cdf473b8f42870791e8f2501903
刘启明 <455043818@qq.com>
2021-05-14 17:50:23 +0800

    hox: 样式打包需单独引入修改vue.config.js配置

--- vue.config.js
+++ vue.config.js
@@ -3,10 +3,7 @@ module.exports = {
     port: 5555,
     proxy: {
       "/offiac": {
-        // target: 'http://localhost:4000',
-        // target: 'http://mozbert.ngrok2.xiaomiqiu.cn',
-        // target: 'https://sykj.huaweiopenlab.com',
-        target: "https://dvs-dev.sunlight-tech.com",
+        target: "http://localhost:4000", // 接口地址
         ws: true,
         changeOrigin: true,
         pathRewrite: {
@@ -14,6 +11,13 @@ module.exports = {
         }
       }
     }
+  },
+  /*
+    默认为true,会将项目中的css文件抽离到单独的css文件中,使用时需要单独import使用
+    设置为false,则会直接一起打包了
+  */
+  css: {
+    extract: false
   },
   //......上面一堆配置
   productionSourceMap: process.env.NODE_ENV === "dev",
