230955b92b47a1d243500595b18aeb63ba167233
刘启明 <455043818@qq.com>
2021-08-14 11:23:40 +0800

    feat: 初始化可视化表单页面布局

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import Table from "../views/Table.vue";
+import Dragger from "../views/Dragger.vue";
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: "/table",
     name: "Table",
     component: Table
+  },
+  {
+    path: "/dragger",
+    name: "Dragger",
+    component: Dragger
   },
   {
     path: "/about",
---
+++ src/views/Dragger.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container">
+        <div class="header">
+            header
+        </div>
+        <div class="body">
+            <div class="left">
+                left
+            </div>
+            <div class="center">
+                    
+            </div>
+            <div class="right">
+                right
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+</script>
+<style scoped="scss">
+    .container {
+        border: 1px solid #EAECEF; 
+        width: 100%;
+        height: 90vh; 
+        display:flex;
+        flex-direction:column;
+    }
+    .header {
+        width: 100%;
+        height: 60px;
+        border-bottom: 1px solid #EAECEF;
+    }
+    .body{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+    }
+    .left {
+        width: 180px;
+        height: calc(90vh - 60px);
+        border-right: 1px solid #EAECEF;
+    }
+    .right {
+        width: 360px;
+        height: calc(90vh- 60px);
+        border-left: 1px solid #EAECEF;
+    }
+</style>
